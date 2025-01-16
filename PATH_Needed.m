b1ec143a072d8ba716a93ba2d72f50d5df7ec283
zhjlu-hpc <zhjlu@outlook.com>
2025-01-16 11:26:08 +0800

    Update PATH_Needed.m

--- PATH_Needed.m
+++ PATH_Needed.m
@@ -1,20 +1,14 @@
 
 %function PATH_Needed
 
-
 addpath Base_/
 addpath Class_/
 addpath tool_functions/
-
 addpath Assemble_Laplace/
 addpath Assemble_Maxwell/
-
 addpath lobpcg_GPU/
-
 addpath IterILU_SAIT/
-
 addpath Material_mark
-
 addpath Mesh/
 
 
